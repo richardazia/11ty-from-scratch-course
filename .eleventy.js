95b802953f07f95d27e6ad975ecd6c3495ed90e3
Richard Azia <richard@main-vision.com>
2023-08-30 15:16:58 +0200

    remove code from eleventy as it is in utils

--- .eleventy.js
+++ .eleventy.js
@@ -2,17 +2,6 @@ module.exports = config => {
   // Set dirs to pass through to the dist folder
   config.addPassthroughCopy('./src/images');
   // Returns work items, sorted by display order
-  config.addCollection('work', collection => {
-    return collection
-      .getFilteredByGlob('./src/work/*.md')
-      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
-  });
-  config.addCollection('featuredWork', collection => {
-    return collection
-      .getFilteredByGlob('./src/work/*.md')
-      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
-      .filter(x => x.data.featured);
-  });
 
   return {
     markdownTemplateEngine: 'njk',
