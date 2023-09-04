993848a872a883bdb14ed94d944a102749d442da
Richard Azia <richard@main-vision.com>
2023-09-04 11:31:54 +0200

    remove gulp related code

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -55,7 +55,22 @@ module.exports = {
     if (limit > 0) {
       filteredItems = filteredItems.slice(0, limit);
     }
-
     return filteredItems;
+  },
+
+  /**
+   * Take an array of keys and return back items that match.
+   * Note: items in the collection must have a key attribute in
+   * Front Matter
+   *
+   * @param {Array} collection 11ty collection
+   * @param {Array} keys collection of keys
+   * @returns {Array} result collection or empty
+   */
+  filterCollectionByKeys(collection, keys) {
+    return collection.filter(x => keys.includes(x.data.key));
   }
+
+
+
 };
